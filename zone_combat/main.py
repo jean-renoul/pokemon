0cbf25a67458db471e2b76511badce1615930acb
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-01-09 13:31:35 +0100

    Ajouts des pokemon et leurs vie dans init

--- zone_combat/main.py
+++ zone_combat/main.py
@@ -1,6 +1,26 @@
 import pygame
 pygame.init()
 
+# Création de la première classe qui va représenter notre pokémon
+class Player:
+    def __init__(self):
+        self.health_salameche = "39"
+        self.health_carapuce = "44"
+        self.health_taupiqueur = "30"
+        self.health_saquedeneu = "65"
+        self.health_chenipan = "45"
+        self.health_pikachu = "35"
+        self.health_tadmorv = "60"
+        self.health_abra = "33"
+        self.health_reptincel = "59"
+        self.health_carabaffe = "64"
+        self.health_triopiqueur = "50"
+        self.health_bouldeneu = "85"
+        self.health_chrysacier = "65"
+        self.health_raichu = "55"
+        self.health_grotadmorv = "80"
+        self.health_kadabra = "53"
+
 # Génération de la fenêtre pygame
 pygame.display.set_caption("Pokemon zone de combat")
 screen = pygame.display.set_mode((850, 531))
