0383ff362c1686b70905ef409ef506adba76db45
maxence-vaisse <maxence.vaisse@laplateforme.io>
2024-01-10 10:45:13 +0100

    Update fichier globals.py

---
+++ globals.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append("Classes")
+from Classes.Pokemon import Pokemon
+
+salameche = Pokemon("Salameche", 1, "Feu", 39, 52, 33)
+carapuce = Pokemon("Carapuce", 1, "Eau", 44, 48, 34)
+taupiqueur = Pokemon("Taupiqueur", 1, "Sol", 30, 52, 35)
+saquedeneu = Pokemon("Saquedeneu", 1, "Plante", 65, 42, 32)
+chenipan = Pokemon("Chenipan", 1, "Insecte", 45, 40, 36)
+pikachu = Pokemon ("Pikachu", 1, "Electrique", 35, 55, 40)
+tadmorv = Pokemon ("Tadmorv", 1, "Poison", 60, 37, 37)
+abra = Pokemon ("Abra", 1, "Psy", 33, 55, 29)
+
+# Pokémon évoluer
+
+reptincel = Pokemon ("Reptincel", 1, "Feu", 59, 72, 53)
+carabaffe = Pokemon ("Carabaffe", 1, "Eau", 64, 68, 54)
+triopiqueur = Pokemon ("Triopiqueur", 1, "Sol", 50, 72, 55)
+bouldeneu = Pokemon ("Bouldeneu", 1, "Plante", 85, 62, 52)
+chrysacier = Pokemon ("Chrysacier", 1, "Insecte", 65, 60, 56)
+raichu = Pokemon ("Raichu", 1, "Electrique", 55, 75, 60)
+grotadmorv = Pokemon ("Grotadmorv", 1, "Poison", 80, 58, 57)
+kadabra = Pokemon ("Kadabra", 1, "Psy", 53, 75, 59)
