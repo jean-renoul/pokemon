347f8b31a24bef319ef8f46795cf2139884ba8d9
jean-renoul <jean.renoul@laplateforme.io>
2024-01-10 10:19:16 +0100

    ajout des starters (pas des évolutions) dans le fichier globals

---
+++ globals.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append("Classes")
+from Classes.Pokemon import Pokemon
+
+salameche = Pokemon("Salameche", 1, "Feu", 39, 52, 33)
+carapuce = Pokemon("Carapuce", 1, "Eau", 44, 48, 34)
+taupiqueur = Pokemon("Taupiqueur", 1, "Sol", 30, 52, 35)
+saquedeneu = Pokemon("Saquedeneu", 1, "Plante", 65, 42, 32)
+chenipan = Pokemon("Chenipan", 1, "Insecte", 45, 40, 36)
+pikachu = Pokemon ("Pikachu", 1, "Electrique", 35, 55, 40)
+tadmorv = Pokemon ("Tadmorv", 1, "Poison", 60, 37, 37)
+abra = Pokemon ("Abra", 1, "Psy", 33, 55, 29)
